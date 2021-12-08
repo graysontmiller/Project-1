a8e7cbe99f0103c11c41cbddf697ca7ba7f05dd5
Kaitlyn Milliken <kaitlynmilliken@gmail.com>
2021-12-08 14:59:24 -0500

    added google api key

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 //TM stands for Ticket Master
 
 var TMKey = "zwO23aREsZiPbHU2oJ7D4IhWyAH6YpEG";
+
+//Google API key
+var GPKey = "AIzaSyBVunudFIdMJK1A1g5tBQEMjMPPkmkZw7I";
+
+
 var TMUrl = "https://app.ticketmaster.com/discovery/v2/";
 
-var searchByZip = function (zip) {
+
+var eventSearchByZip = function (zip) {
     //calls to ticket master API to get all events upcoming for this zip code
     fetch(TMUrl + "events.json?postalCode=" + zip + "&apikey=" + TMKey)
         //after returning a promise, we are processing that response and transforming to JSON data
         .then(Response => Response.json()).then((data) => {
             //eliminates unneccassary data from our collection
             data = data._embedded.events;
-            for (let event in data){
+            for (let event in data) {
                 //loops through ever object in the returned event array to log data
                 console.log(data[event]);
             }
@@ -21,13 +27,13 @@ var searchByZip = function (zip) {
 
 //Alternatively, we can search by city instead as events are harder to find vs food
 
-var searchByCity = function(city){
+var eventSearchByCity = function (city) {
     fetch(TMUrl + "events.json?city=" + city + "&apikey=" + TMKey)
         //after returning a promise, we are processing that response and transforming to JSON data
         .then(Response => Response.json()).then((data) => {
             //eliminates unneccassary data from our collection
             data = data._embedded.events;
-            for (let event in data){
+            for (let event in data) {
                 //loops through ever object in the returned event array to log data
                 console.log(data[event]);
             }
@@ -37,10 +43,11 @@ var searchByCity = function(city){
 };
 
 
+
 //search by zip by passing zip code through 
-searchByZip("28262");
+//eventSearchByZip("28262");
 
 //search by city by passing city name
-searchByCity("Charlotte");
+//eventSearchByCity("Charlotte");
 
 
